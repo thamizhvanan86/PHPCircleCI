2dd7c31807f6d706f25982336d0d41759bad3eee
Thamizhvanan Elangovan <Thamizhvanan.E@cit.congruentindia.com>
2019-12-10 18:51:38 +0530

    updated php files and removed Dockerfile

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM php:7.1-apache
-COPY index.php /var/www/html
-EXPOSE 80
--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
 echo "Hello, World from Docker! <br>";
 echo "Hello ECS! <br>";
-echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 ?>
